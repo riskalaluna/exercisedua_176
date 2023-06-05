9ca6e334a51deecbd4d760cdddfe82148ce5f6d5
riskalaluna <riskalaluna@gmail.com>
2023-06-05 13:44:11 +0700

    class penerbit

--- exercisedua_176/exercisedua_176.cpp
+++ exercisedua_176/exercisedua_176.cpp
@@ -1,3 +1,18 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+
+class penerbit;
+class pengarang {
+public:
+	string nama;
+	vector<penerbit*> daftar_penerbit;
+	pengarang(string pNama) :nama(pNama) {
+		cout << "Pengarang \"" << nama << "\" ada\n";
+	}
+	~pengarang() {
+		cout << "Pengarang \"" << nama << "\" tidak ada\n";
+	}
+	void tambahPenerbit(penerbit*);
+	void cetakPenerbit();
+};
